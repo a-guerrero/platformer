3cb0cea1bc1565c681fb1651b7cb0b5be0caed9f
a-guerrero <a.guerrero.g870@gmail.com>
2016-12-15 22:40:21 -0600

    chore(node): add webpack source map support

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 module.exports = {
     entry: './src/scripts/app.ts',
     output: {
-        path: './build',
+        path: __dirname + '/build/scripts',
         filename: 'app.js'
     },
     resolve: {
         // Add `.ts` and `.tsx` as a resolvable extension.
         extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
     },
+
+    // Source maps support
+    devtool: 'source-map',
+
     module: {
         loaders: [
-            { test: /\.tsx?$/, loader: 'ts-loader' }
+            { test: /\.ts$/, loader: 'ts-loader' }
         ]
     }
 };
