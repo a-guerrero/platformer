a0d1cd23e5087111d3ead09a60e062a905e66e03
a-guerrero <a.guerrero.g870@gmail.com>
2017-01-01 17:32:02 -0600

    refactor(app/scripts): instatiate Platformer in app.ts

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,1 +1,9 @@
-console.log('app.ts');
+import { Platformer } from './game/Platformer';
+
+let canvas = <HTMLCanvasElement>document.getElementById('js-stage');
+let platformer = new Platformer({
+    elem: canvas,
+    ctx: canvas.getContext('2d'),
+    width: 512,
+    height: 288
+});
