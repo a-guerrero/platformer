c1aeac31986dc42541217de9d7d90d75846ff2f8
a-guerrero <a.guerrero.g870@gmail.com>
2017-01-02 14:33:43 -0600

    refactor(app/scripts/game): add auto jump to Antagonist

--- app/scripts/game/Antagonist.ts
+++ app/scripts/game/Antagonist.ts
@@ -2,18 +2,48 @@ import { Character } from './Character';
 
 export class Antagonist extends Character {
 
+    canJump: boolean;
+    jumpWait: number;
+    private jumpFrameCount: number;
+
     constructor(public context: CanvasRenderingContext2D) {
 
         super(context);
 
-        this.speed = 0.25;
+        // Super defaults override
+        this.ySpeed = 7;
+        this.xSpeed = 0.25;
         this.fillStyle = 'red';
+
+        // Defaults
+        this.canJump = false;
+        this.jumpWait = 25;
+        this.jumpFrameCount = 0;
+    }
+
+    update(): this {
+
+        if (!this.isJumping) {
+            // Wait some frames before jumping
+            if (this.jumpFrameCount++ === this.jumpWait) {
+                // Jump
+                this.jump();
+                // Reset jump frame count
+                this.jumpFrameCount = 0;
+            }
+        }
+
+        // Update super
+        super.update();
+
+        return this;
     }
 
     collisionHandler(side: 'top' | 'right' | 'bottom' | 'left', depth: number): this {
 
         super.collisionHandler(side, depth);
 
+        // Keep moving after side collision
         if (!this.isDeath && (side === 'right' || side === 'left')) {
 
             this.isMovingLeft = side === 'right';
